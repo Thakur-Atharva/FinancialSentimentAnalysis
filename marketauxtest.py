6e6e5ca0458b871c9ca2250e43410d7f40d2d8bc
Rishi Mantri <rishimantri795@gmail.com>
2023-10-29 14:29:02 -0400

    added data to csv file

--- marketauxtest.py
+++ marketauxtest.py
@@ -6,8 +6,11 @@ import datetime
 conn = http.client.HTTPSConnection('api.marketaux.com')
 
 # Read the company symbols from the excel file
-df = pd.read_excel('SP_500_Companies.xlsx')
+df = pd.read_excel('S&P - first 50.xlsx')
 symbols = df['Symbol'].tolist()
+
+# Create an empty list to store the data
+company_data_list = []
 
 # Loop through each symbol
 for symbol in symbols:
@@ -18,7 +21,7 @@ for symbol in symbols:
         'symbols': symbol,
         'limit': 3,
         'search': 'stock + {}'.format(symbol),
-        'published_after': (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M')
+        'published_after': (datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%dT%H:%M')
     })
 
     conn.request('GET', '/v1/news/all?{}'.format(params))
@@ -33,9 +36,13 @@ for symbol in symbols:
 
     # Store the data in a dictionary
     company_data = {'symbol': symbol, 'titles': titles, 'urls': urls}
-
-    print(company_data)
-
-
-
     
+    # Append the data to the list
+    company_data_list.append(company_data)
+
+# Create a pandas dataframe from the list
+df = pd.DataFrame(company_data_list)
+
+# Write the dataframe to a CSV file
+df.to_csv('company_data.csv', index=False)
+
