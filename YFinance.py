161bcf458ef1b7ea93cbf4579e61f5a0bd405616
Rishi Mantri <rishimantri795@gmail.com>
2023-11-04 17:21:09 -0400

    working

--- YFinance.py
+++ YFinance.py
@@ -14,3 +14,18 @@ URL = "https://finance.yahoo.com/m/13f36da8-1059-40c1-a5ad-f6d57bc1a60b/new-repo
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.text.replace(". ", ".\n"))
+
+
+#from github
+
+from bs4 import BeautifulSoup
+import requests
+URL = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+for link in soup.find_all('a', href = True):
+    print (link.get('href'))
+URL = "https://finance.yahoo.com/news/true-value-always-prevails-against-technological-advancement-141902215.html"
+page2 =requests.get(URL)
+soup = BeautifulSoup(page2.content, 'html.parser')
+print(soup.text)
